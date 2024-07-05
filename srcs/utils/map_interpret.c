dba834004829802d910a8f958afe6da61672c300
Henni <henni.wie@gmail.com>
2024-07-05 13:20:41 +0200

    blue theme and imgage paths

--- srcs/utils/map_interpret.c
+++ srcs/utils/map_interpret.c
@@ -3,41 +3,41 @@
 // need to look int mlx_texture_t struct and calculate pixels to distance
 
 
-// void	get_textures(t_game *game)
-// {
-// 	game->tex.NO = mlx_load_png(game->look.NO);
-// 	game->tex.SO = mlx_load_png(game->look.SO);
-// 	game->tex.WE = mlx_load_png(game->look.WE);
-// 	game->tex.EA = mlx_load_png(game->look.EA);
-// 	if (!(game->tex.NO) || !(game->tex.SO) || !(game->tex.WE)
-// 		|| !(game->tex.EA))
-// 	{
-// 		printf("Error texture load\n");
-// 		exit(1);
-// 	}
-// }
+void	get_textures(t_game *game)
+{
+	game->tex.NO = mlx_load_png(game->look.NO);
+	game->tex.SO = mlx_load_png(game->look.SO);
+	game->tex.WE = mlx_load_png(game->look.WE);
+	game->tex.EA = mlx_load_png(game->look.EA);
+	if (!(game->tex.NO) || !(game->tex.SO) || !(game->tex.WE)
+		|| !(game->tex.EA))
+	{
+		printf("Error texture load\n");
+		exit(1);
+	}
+}
 
 
-// void	get_img(t_game *game)
-// {
-// 	get_textures(game);
-// 	game->img.NO = mlx_texture_to_image(game->mlx_ptr,
-// 			game->tex.NO);
-// 	game->img.SO = mlx_texture_to_image(game->mlx_ptr,
-// 			game->tex.SO);
-// 	game->img.WE = mlx_texture_to_image(game->mlx_ptr,
-// 			game->tex.WE);
-// 	game->img.EA = mlx_texture_to_image(game->mlx_ptr,
-// 			game->tex.EA);
-// 	clean_texture(game);
-// }
+void	get_img(t_game *game)
+{
+	get_textures(game);
+	game->image.NO = mlx_texture_to_image(game->mlx_ptr,
+			game->tex.NO);
+	game->image.SO = mlx_texture_to_image(game->mlx_ptr,
+			game->tex.SO);
+	game->image.WE = mlx_texture_to_image(game->mlx_ptr,
+			game->tex.WE);
+	game->image.EA = mlx_texture_to_image(game->mlx_ptr,
+			game->tex.EA);
+	clean_texture(game);
+}
 
 void	clean_texture(t_game *game)
 {
-	// mlx_delete_texture(game->tex.NO);
-	// mlx_delete_texture(game->tex.SO);
-	// mlx_delete_texture(game->tex.WE);
-	// mlx_delete_texture(game->tex.EA);
+	mlx_delete_texture(game->tex.NO);
+	mlx_delete_texture(game->tex.SO);
+	mlx_delete_texture(game->tex.WE);
+	mlx_delete_texture(game->tex.EA);
 	mlx_delete_texture(game->tex.wall);
 	mlx_delete_texture(game->tex.player);
 	mlx_delete_texture(game->tex.target);
