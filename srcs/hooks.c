dc17609fc8b20830cdd71a423437979148854119
Henni <henni.wie@gmail.com>
2024-07-16 12:15:06 +0200

    keys seperated

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -25,70 +25,71 @@ void	key_hook(mlx_key_data_t keydata, void *param)
 	map_data = &game->map_data;
 	if (keydata.action == MLX_PRESS)
 	{
-		if (keydata.key == MLX_KEY_ESCAPE)
-		{
-			free_data(game);
-			mlx_close_window(game->mlx_ptr);
-		}
-		if (keydata.key == MLX_KEY_SPACE)
-		{
-			open_doors(game, render_data, map_data);
-			ft_putendl_fd("SPACE", STDERR_FILENO);
-			check_sprites(game, render_data, 0);
-		}
-		if (keydata.key == MLX_KEY_X)
-		{
-			get_target(game, map_data);
-			ft_putendl_fd("X", STDERR_FILENO);
-			check_sprites(game, render_data, 1);
-		}
-		if (keydata.key == MLX_KEY_Z)
-		{
-			get_air(game, map_data);
-			ft_putendl_fd("Z", STDERR_FILENO);
-			check_sprites(game, render_data, 2);
-		}
+		keys_act(game, keydata);
 		render_data->flag_render = 1; //was ist das???
 	}
 	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT)
-			if (keydata.key == MLX_KEY_LEFT)
-		{
-			rotate(render_data, -1);
-		}
-		if (keydata.key == MLX_KEY_RIGHT)
-		{
-			rotate(render_data, 1);
-		}
-
+		keys_rotate(keydata, render_data);
 	if (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT
 		|| keydata.action == MLX_RELEASE)
+		keys_walk(game, keydata);
+}
+
+void	keys_walk(t_game *game, mlx_key_data_t keydata)
+{
+	t_render_data	*render_data;
+	t_map_data		*map_data;
+
+	render_data = &game->render_data;
+	map_data = &game->map_data;
+	if (keydata.key == MLX_KEY_W)
+		player_n1_move(game, map_data, render_data, 1);
+	if (keydata.key == MLX_KEY_S)
+		player_n1_move(game, map_data, render_data, -1);
+	if (keydata.key == MLX_KEY_A)
+		player_n1_sideways(game, map_data, render_data, 1);
+	if (keydata.key == MLX_KEY_D)
+		player_n1_sideways(game, map_data, render_data, -1);
+	if (keydata.key == MLX_KEY_ESCAPE)
+		mlx_close_window(game->mlx_ptr);
+	if (game->render_data.count_oxy_caught != 0)
+		create_bubbles(game);
+}
+
+void	keys_rotate(mlx_key_data_t keydata, t_render_data *render_data)
+{
+	if (keydata.key == MLX_KEY_LEFT)
+		rotate(render_data, -1);
+	if (keydata.key == MLX_KEY_RIGHT)
+		rotate(render_data, 1);
+}
+
+void	keys_act(t_game *game, mlx_key_data_t keydata)
+{
+	t_render_data	*render_data;
+	t_map_data		*map_data;
+
+	render_data = &game->render_data;
+	map_data = &game->map_data;
+	if (keydata.key == MLX_KEY_ESCAPE)
 	{
-		if (keydata.key == MLX_KEY_W)
-		{
-			player_n1_move(game, map_data, render_data, 1);
-			ft_putendl_fd("W", STDERR_FILENO);
-		}
-		if (keydata.key == MLX_KEY_S)
-		{
-			player_n1_move(game, map_data, render_data, -1);
-			ft_putendl_fd("S", STDERR_FILENO);
-		}
-		if (keydata.key == MLX_KEY_A)
-		{
-			player_n1_sideways(game, map_data, render_data, 1);
-			ft_putendl_fd("A", STDERR_FILENO);
-		}
-		if (keydata.key == MLX_KEY_D)
-		{
-			player_n1_sideways(game, map_data, render_data, -1);
-			ft_putendl_fd("D", STDERR_FILENO);
-		}
-		if (keydata.key == MLX_KEY_ESCAPE)
-		{
-			mlx_close_window(game->mlx_ptr);
-		}
-		if (game->render_data.count_oxy_caught != 0)
-			create_bubbles(game);
+		free_data(game);
+		mlx_close_window(game->mlx_ptr);
+	}
+	if (keydata.key == MLX_KEY_SPACE)
+	{
+		open_doors(game, render_data, map_data);
+		ft_putendl_fd("SPACE", STDERR_FILENO);
+	}
+	if (keydata.key == MLX_KEY_X)
+	{
+		get_target(game, map_data);
+		ft_putendl_fd("X", STDERR_FILENO);
+	}
+	if (keydata.key == MLX_KEY_Z)
+	{
+		get_air(game, map_data);
+		ft_putendl_fd("Z", STDERR_FILENO);
 	}
 }
 
